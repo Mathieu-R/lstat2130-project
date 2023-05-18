f8bfdfbf715288fbda95dccf7ebe1f52e27b7901
Mathieu-R <mathieu0709@gmail.com>
2023-05-18 16:31:37 +0200

    update

--- report/code/q2c.r
+++ report/code/q2c.r
@@ -3,13 +3,13 @@ log.prior <- function(theta) {
   theta1 <- theta[2]
   theta2 <- theta[3]
   
-  prior.theta0 <- dunif(theta0, -100, 100, log = TRUE)
-  prior.theta1 <- dunif(theta1, -100, 100, log = TRUE)
-  prior.theta2 <- dunif(theta2, -100, 100, log = TRUE)
+  prior.theta0 <- dnorm(theta0, mean = 0, sd = 10, log = TRUE)
+  prior.theta1 <- dnorm(theta1, mean = 0, sd = 10, log = TRUE)
+  prior.theta2 <- dnorm(theta2, mean = 0, sd = 10, log = TRUE)
   
   return(prior.theta0 + prior.theta1 + prior.theta2)
 }
 
 log.posterior <- function(theta, t, experiment) {
   log.likelihood(theta, t, experiment) + log.prior(theta)
-}
+} 
